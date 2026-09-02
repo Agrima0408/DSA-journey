7b50415c36d3ac54c8a6d44582dfbdecbe9f28af
Agrima Agarwal <agrima.ag04@gmail.com>
2026-09-03 00:11:12 +0530

    Create Majority_Element.java

---
+++ Arrays_otherPlatforms/Majority_Element/Majority_Element.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class MajorityElement{
+
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++){
+
+            arr[i]=sc.nextInt();
+
+        }
+
+        for(int i=0;i<n;i++){
+
+            int count=0;
+
+            for(int j=0;j<n;j++){
+
+                if(arr[i]==arr[j]){
+
+                    count++;
+
+                }
+
+            }
+
+            if(count>n/2){
+
+                System.out.println(arr[i]);
+
+                break;
+
+            }
+
+            else{
+
+                System.out.println("No majority element");
+
+                break;
+
+            }
+
+        }
+
+    }
+
+}
