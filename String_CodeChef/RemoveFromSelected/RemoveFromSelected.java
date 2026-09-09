c57059fc9b35f3157014e9513b88ddae5837642e
Agrima Agarwal <agrima.ag04@gmail.com>
2026-09-09 22:58:32 +0530

    Refactor main method to use StringBuilder for output

--- String_CodeChef/RemoveFromSelected/RemoveFromSelected.java
+++ String_CodeChef/RemoveFromSelected/RemoveFromSelected.java
@@ -1,33 +1,27 @@
 import java.util.Scanner;
 
 public class removeFromSelected {
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        String s1 = sc.next();
-
-        String s2 = sc.next();
-
-        String ans ="";
-
-        for(int i=0;i<s1.length();i++){
-
-            for(int j=0;j<s2.length();j++){
-
-                if(s1.charAt(i) != s2.charAt(j)){
-
-                 ans+=s1.charAt(i);
-
-                }
-
-            }
-
-        }
-
-        System.out.println(ans);
-
-    }
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        StringBuilder ans = new StringBuilder();
+      
+        for(int i=0;i<s1.length();i++){
+            boolean found = false;
+          
+            for(int j=0;j<s2.length();j++){
+                if(s1.charAt(i) == s2.charAt(j)){
+                    found = true;
+                    break;
+                }
+            }
+          
+            if (!found) {
+                ans.append(s1.charAt(i));
+            }
+        }
+      
+        System.out.println(ans.toString());
+    }
 }
